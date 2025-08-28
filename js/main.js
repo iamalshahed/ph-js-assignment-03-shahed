68e122814225b235a6d7b2eb6b42668268137e91
iamalshahed <shahedevise@gmail.com>
2025-08-28 16:25:22 +0600

    add call buttons functionalities, Show an alert with a message including the service name and number and also Each call will cut 20 coins. Reduce Coin after each click.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
 let loveValue = Number(document.getElementById("love__value").innerText);
 const heartIcons = document.getElementsByClassName("heart__icon");
-// console.log(loveValue);
 
+const btnsCall = document.getElementsByClassName("btn__call");
+
+// when clicked heart icon
 for (let heartIcon of heartIcons) {
   heartIcon.addEventListener("click", function () {
     loveValue++;
     document.getElementById("love__value").innerText = loveValue;
   });
 }
+
+// when clicked call button
+for (let btnCall of btnsCall) {
+  btnCall.addEventListener("click", function () {
+    // variables
+    let coinValue = Number(document.getElementById("coin__value").innerText);
+    let getCoinValue = document.getElementById("coin__value");
+    const serviceTitle =
+      btnCall.parentNode.parentNode.childNodes[7].childNodes[1].innerText;
+    const serviceNumber =
+      btnCall.parentNode.parentNode.childNodes[11].childNodes[1].innerText;
+
+    if (coinValue < 20) {
+      alert(
+        "❌ You don't have enough coins. You need at least 20 coins to make a call."
+      );
+    } else {
+      alert(`📞 Calling ${serviceTitle} ${serviceNumber} ...`);
+      let updateCoinValue = (coinValue -= 20);
+      getCoinValue.innerText = updateCoinValue;
+    }
+  });
+}
+
+/*
+
+
+    if (coinValue <= 100) {
+      console.log(
+        btnCall.parentNode.parentNode.childNodes[7].childNodes[1].innerText
+      );
+
+      console.log(
+        btnCall.parentNode.parentNode.childNodes[11].childNodes[1].innerText
+      );
+    } else {
+      alert("nope");
+    }
+      */
