72dfa1b96d04c33d4638db8be7aa4dfac9ff145b
iamalshahed <shahedevise@gmail.com>
2025-08-28 13:42:25 +0600

    count love value functionality added successfully

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("connected!");
+let loveValue = Number(document.getElementById("love__value").innerText);
+const heartIcons = document.getElementsByClassName("heart__icon");
+// console.log(loveValue);
+
+for (let heartIcon of heartIcons) {
+  heartIcon.addEventListener("click", function () {
+    loveValue++;
+    document.getElementById("love__value").innerText = loveValue;
+  });
+}
